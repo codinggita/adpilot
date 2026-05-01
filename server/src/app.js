bb25b40fc31e1a81c0ce93ab6e6c2a14f48aa91d
atulXdev <atul.kumar.singh.cg@gmail.com>
2026-05-01 11:12:38 +0530

    added render base url

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   credentials: true,
 }));
 
